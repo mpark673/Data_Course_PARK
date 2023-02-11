3e24f998742eb5087b03d04e589bd59613c7cd76
mpark673 <masonpark673@gmail.com>
2023-02-10 17:44:17 -0700

    Assignment 6

---
+++ Assignments/Assignment_6/Assignment_6_masonpark.R
@@ -0,0 +1,41 @@
+# Assignment 6
+
+# Library and read, this was given
+library(tidyverse)
+library(gganimate)
+df <- read_csv("../../Data/BioLog_Plate_Data.csv")
+
+# Cleaning Data into tidy (long) form
+
+df <- df %>% 
+  pivot_longer(c(`Hr_24`, `Hr_48`,`Hr_144`), names_to = "Time", values_to = "Absorbance")
+
+# Creating a new column that shows sample type
+
+df$Type <- df$`Sample ID`
+
+df <- df %>% 
+  mutate(Type=recode(Type,Clear_Creek="Water",Soil_1="Soil",Soil_2="Soil",Waste_Water="Water"))
+
+# Generate a plot matching the one on the website
+
+df <- df %>% 
+  mutate(Time=recode(Time,Hr_24=24,Hr_48=48,Hr_144=144))
+df
+df %>% 
+  filter(Dilution==0.1) %>% 
+  ggplot(aes(x=Time,y=Absorbance,color=Type))+
+  facet_wrap(~Substrate)+
+  geom_smooth(se=F)+
+  theme_minimal()
+
+# Generate an animated Plot
+
+p <- df %>% 
+  filter(Substrate=="Itaconic Acid") %>%
+  group_by(`Sample ID`,Time,Dilution) %>% 
+  summarize(Mean_absorbance=mean(Absorbance,na.rm = T)) %>% 
+  ggplot(aes(x=Time,y=Mean_absorbance,color=`Sample ID`))+
+  
+  geom_line()
+p+facet_wrap(~Dilution)+transition_time(Time)
